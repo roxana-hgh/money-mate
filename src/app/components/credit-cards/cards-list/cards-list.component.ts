25e842e8486326680589b105285c5c00872d8f67
roxana-hgh <haghgoo.roxana@gmail.com>
2024-11-15 16:19:33 +0330

    cards slider updated with owl

--- src/app/components/credit-cards/cards-list/cards-list.component.ts
+++ src/app/components/credit-cards/cards-list/cards-list.component.ts
@@ -1,4 +1,4 @@
-import { Component  } from '@angular/core';
+import { Component } from '@angular/core';
 import { AccountsService } from '../../../services/accounts.service';
 import { Account } from '../../../models/account.model';
 import { OwlOptions } from 'ngx-owl-carousel-o';
@@ -7,30 +7,29 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
   selector: 'app-cards-list',
   templateUrl: './cards-list.component.html',
   styleUrl: './cards-list.component.scss',
-
 })
 export class CardsListComponent {
-  accounts !: Account[]
+  accounts!: Account[];
 
   customOptions: OwlOptions = {
     loop: false,
-
+    margin: 10,
     dots: true,
     navSpeed: 700,
-   
+
     responsive: {
       0: {
-        items: 1
+        items: 1,
       },
       500: {
-        items: 2
+        items: 2,
       },
       940: {
-        items: 3
-      }
+        items: 3,
+      },
     },
-    nav: false
-  }
+    nav: false,
+  };
 
   constructor(private accountsService: AccountsService) {}
 
@@ -38,12 +37,10 @@ export class CardsListComponent {
     this.accountsService.getAccounts().subscribe((res: Account[]) => {
       this.accounts = res;
       this.accounts = this.accounts.sort((a, b) => {
-        return (new Date(b.created_at).getTime() - new Date(a.created_at).getTime()) as number;
+        return (new Date(b.created_at).getTime() -
+          new Date(a.created_at).getTime()) as number;
       });
       //console.log("acc", this.accounts);
     });
   }
-
-  
-
 }
