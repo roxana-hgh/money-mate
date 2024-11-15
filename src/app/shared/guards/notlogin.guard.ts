9af487309dc74352169b9b53d17ed72b1811d7b6
roxana-hgh <haghgoo.roxana@gmail.com>
2024-11-15 16:33:57 +0330

    notlogin gurad updated

--- src/app/shared/guards/notlogin.guard.ts
+++ src/app/shared/guards/notlogin.guard.ts
@@ -2,19 +2,20 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router, CanActivateChild } from '@angular/router';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
+import { AuthService } from '../../services/auth.service';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotloginGuard implements CanActivate {
-  constructor(private router: Router){}
+  constructor(private router: Router, private authService: AuthService){}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> | boolean | UrlTree {
-    const isAuthenticated = localStorage.getItem('userData') ;
+    const isAuthenticated = this.authService.CheckAuthenticated();
 
     
       if (isAuthenticated) {
